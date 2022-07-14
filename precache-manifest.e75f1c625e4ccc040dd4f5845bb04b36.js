f7a2262968d9520032a2ed2964e061648d621738
beldeeb <bassel_eldeeb@knights.ucf.edu>
2022-07-14 16:31:45 -0400

    Updates

---
+++ precache-manifest.e75f1c625e4ccc040dd4f5845bb04b36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "98027cab478658d41638",
+    "url": "/SweatCheck/static/css/main.f90fd3cf.chunk.css"
+  },
+  {
+    "revision": "98027cab478658d41638",
+    "url": "/SweatCheck/static/js/main.98027cab.chunk.js"
+  },
+  {
+    "revision": "f8720af5a01c79588fba",
+    "url": "/SweatCheck/static/css/1.3ee4559a.chunk.css"
+  },
+  {
+    "revision": "f8720af5a01c79588fba",
+    "url": "/SweatCheck/static/js/1.f8720af5.chunk.js"
+  },
+  {
+    "revision": "97fd8b3154175daadebb",
+    "url": "/SweatCheck/static/js/runtime~main.97fd8b31.js"
+  },
+  {
+    "revision": "bbcc662a58e1aed83b10fbbdea98bb23",
+    "url": "/SweatCheck/static/media/logo.bbcc662a.png"
+  },
+  {
+    "revision": "faeb0f2555fa802ef85594c1ab0b5c66",
+    "url": "/SweatCheck/static/media/bg.faeb0f25.png"
+  },
+  {
+    "revision": "3b13913e22909934ea5a63561e24e5f3",
+    "url": "/SweatCheck/index.html"
+  }
+];
